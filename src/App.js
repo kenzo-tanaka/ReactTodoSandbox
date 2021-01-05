c54a7413e5c1224f034196fedfe67be74152106b
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-05 14:55:53 +0900

    feat: フィルタリングされたtodoのみを表示する

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
@@ -13,12 +13,27 @@ const App = () => {
     setFilter(event.target.value);
   };
 
+  const filterdTodos = useMemo(() => {
+    switch (filter) {
+      case "active":
+        return todos.filter((todo) => !todo.completed);
+      case "completed":
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  }, [todos, filter]);
+
   return (
     <div>
       <h2>Todo List</h2>
       <TodoFilter selectedFilter={filter} handleFilter={handleFilter} />
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoList
+        todos={filterdTodos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 };
